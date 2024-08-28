8dd5bd1b7958ad4d5051452466bb7cfcc2f77e1a
Orstedz <giaminh2042004@gmail.com>
2024-08-28 20:47:25 +0700

    Add MovieProvider.jsx to reuse code and complete v1

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
+import { useContext } from "react";
 import propTypes from "prop-types";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import Modal from "react-modal";
-import YouTube from "react-youtube";
-
-const opts = {
-  height: "390",
-  width: "640",
-  playerVars: {
-    // https://developers.google.com/youtube/player_parameters
-    autoplay: 1,
-  },
-};
+import { MovieContext } from "../context/MovieProvider";
 
 const responsive = {
   superLargeDesktop: {
@@ -34,31 +24,7 @@ const responsive = {
 };
 
 const MovieList = ({ title, data }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [trailerKey, setTrailerKey] = useState("");
-
-  const handleTrailer = async (id) => {
-    setTrailerKey("");
-    try {
-      const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-        },
-      };
-
-      const movieKey = await fetch(url, options);
-      const data = await movieKey.json();
-
-      setTrailerKey(data.results[0].key);
-      setModalIsOpen(true);
-    } catch (error) {
-      setModalIsOpen(false);
-      console.log(error);
-    }
-  };
+  const { handleTrailer } = useContext(MovieContext);
 
   return (
     <div className="text-white p-10 mb-10">
@@ -88,28 +54,6 @@ const MovieList = ({ title, data }) => {
             </div>
           ))}
       </Carousel>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        style={{
-          overlay: {
-            position: "fixed",
-            zIndex: 9999,
-          },
-          content: {
-            top: "50%",
-            left: "50%",
-            right: "auto",
-            bottom: "auto",
-            marginRight: "-50%",
-            transform: "translate(-50%, -50%)",
-          },
-        }}
-        contentLabel="Example Modal"
-      >
-        <YouTube videoId={trailerKey} opts={opts} />;
-      </Modal>
     </div>
   );
 };
