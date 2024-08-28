da31d2d16eb2a64ebb663b7204f984b03372986a
Orstedz <giaminh2042004@gmail.com>
2024-08-28 16:53:21 +0700

    Make MovieList interactive with movie trailer

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,17 +5,6 @@ import "react-multi-carousel/lib/styles.css";
 import Modal from "react-modal";
 import YouTube from "react-youtube";
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
-
 const opts = {
   height: "390",
   width: "640",
@@ -45,7 +34,31 @@ const responsive = {
 };
 
 const MovieList = ({ title, data }) => {
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [trailerKey, setTrailerKey] = useState("");
+
+  const handleTrailer = async (id) => {
+    setTrailerKey("");
+    try {
+      const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
+      };
+
+      const movieKey = await fetch(url, options);
+      const data = await movieKey.json();
+
+      setTrailerKey(data.results[0].key);
+      setModalIsOpen(true);
+    } catch (error) {
+      setModalIsOpen(false);
+      console.log(error);
+    }
+  };
 
   return (
     <div className="text-white p-10 mb-10">
@@ -53,7 +66,11 @@ const MovieList = ({ title, data }) => {
       <Carousel responsive={responsive} className="flex items-center space-x-2">
         {data.length > 0 &&
           data.map((item) => (
-            <div key={item.id} className="w-[200px] h-[300px] relative group">
+            <div
+              key={item.id}
+              className="w-[200px] h-[300px] relative group"
+              onClick={() => handleTrailer(item.id)}
+            >
               <div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
                 <div className="absolute top-0 left-0 w-full h-full bg-black/40" />
                 <img
@@ -73,11 +90,24 @@ const MovieList = ({ title, data }) => {
 
       <Modal
         isOpen={modalIsOpen}
-        onRequestClose={() => setIsOpen(false)}
-        style={customStyles}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            position: "fixed",
+            zIndex: 9999,
+          },
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
         contentLabel="Example Modal"
       >
-        <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />;
+        <YouTube videoId={trailerKey} opts={opts} />;
       </Modal>
     </div>
   );
