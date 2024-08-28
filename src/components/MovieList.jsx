de624a42f23336bff3c94b9785b7b6392c6bc87d
Orstedz <giaminh2042004@gmail.com>
2024-08-28 12:55:45 +0700

    Import TMDB API and generate MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,37 @@
-import PropTypes from "prop-types";
+import propTypes from "prop-types";
+import ImgTemp from "../assets/temp-1.jpeg";
 
-const MovieList = ({ title }) => {
-  return <div className="text-white p-10 mb-10">{title}</div>;
+const MovieList = ({ title, data }) => {
+  return (
+    <div className="text-white p-10 mb-10">
+      <h2 className="uppercase text-xl mb-4">{title}</h2>
+      <div className="flex items-center space-x-4">
+        {data.length > 0 &&
+          data.map((item) => (
+            <div key={item.id} className="w-[200px] h-[300px] relative group">
+              <div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
+                <div className="absolute top-0 left-0 w-full h-full bg-black/40" />
+                <img
+                  src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`}
+                  alt="item.title"
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute bottom-4 left-2">
+                  <p className="uppercase text-md">
+                    {item.title || item.original_title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
-MovieList.PropTypes = {
-  title: PropTypes.string,
+MovieList.propTypes = {
+  title: propTypes.string.isRequired,
+  data: propTypes.array,
 };
 
 export default MovieList;
